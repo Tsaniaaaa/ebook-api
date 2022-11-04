864b241d47d9845dc084b2facbe68f11380b4f6a
Tsaniaaaa <tiaratsaniaa@gmail.com>
2022-11-04 11:11:14 +0700

    Auth

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('me', [AuthController::class, 'me']);
 
 Route::resource('books', BookController::class)->except([
@@ -31,3 +35,10 @@ Route::resource('books', BookController::class)->except([
 Route::resource('author', AuthorController::class)->except([
     'create', 'edit'
 ]);
+
+//proctected routes
+Route::middleware('auth:sanctum')->group(function() {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
